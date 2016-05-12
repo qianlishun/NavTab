a60ca1785bf94bcc1670220a71381b6b14d45601
Mr.Q <qianlishun@mrq-macbook-pro.local>
2016-05-12 10:32:41 +0800

    Initial Commit

---
+++ QLSNavTab/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QLSNavTab
+//
+//  Created by Mr.Q on 16/5/12.
+//  Copyright © 2016年 QLS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QLSNavTab/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QLSNavTab
+//
+//  Created by Mr.Q on 16/5/12.
+//  Copyright © 2016年 QLS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
