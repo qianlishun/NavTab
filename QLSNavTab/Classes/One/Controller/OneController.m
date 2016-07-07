28b066b9c1d3c102339d9a5476a2fa47d828cb5b
Mr.Q <qianlishun@mrq-macbook-pro.local>
2016-07-07 15:04:12 +0800

    修复模态弹出的 bug

--- QLSNavTab/Classes/One/Controller/OneController.m
+++ QLSNavTab/Classes/One/Controller/OneController.m
@@ -8,6 +8,7 @@
 
 #import "OneController.h"
 #import "FourController.h"
+#import "ThreeController.h"
 
 @interface OneController ()
 
@@ -19,19 +20,12 @@
     [super viewDidLoad];
 
     self.title = @"My";
-    
-    UIButton *button=[UIButton buttonWithType:UIButtonTypeCustom];
-    button.frame=CGRectMake(0, 0, 200, 60);
-    button.center=self.view.center;
 
-    button.backgroundColor=[UIColor colorWithRed:arc4random_uniform(255)/255.0 green:arc4random_uniform(255)/255.0 blue:arc4random_uniform(255)/255.0 alpha:1];
+    UIButton *btn = [self buttonWithAction:@selector(jumpToFour) andTitle:@"push跳转"];
+    btn.center = CGPointMake(self.view.center.x, 100);
 
-
-    [button setTitle:@"点击跳转" forState:UIControlStateNormal];
-    [self.view addSubview:button];
-
-    [button addTarget:self action:@selector(jumpToFour) forControlEvents:UIControlEventTouchUpInside];
-
+    UIButton *btn2 = [self buttonWithAction:@selector(presentToFour) andTitle:@"模态弹出"];
+    btn2.center = CGPointMake(self.view.center.x, 200);
 
 }
 
@@ -39,5 +33,27 @@
     [self.navigationController pushViewController:[[FourController alloc]init] animated:YES];
 }
 
+- (void)presentToFour{
+
+    UINavigationController *nav = [[UINavigationController alloc]initWithRootViewController:[ThreeController new]];
+
+    [self presentViewController:nav animated:YES completion:nil];
+    
+}
+
+- (UIButton *)buttonWithAction:(SEL)action andTitle:(NSString *)title{
+
+    UIButton *button=[UIButton buttonWithType:UIButtonTypeCustom];
+    button.frame=CGRectMake(0, 0, 200, 60);
+
+    button.backgroundColor=[UIColor colorWithRed:arc4random_uniform(255)/255.0 green:arc4random_uniform(255)/255.0 blue:arc4random_uniform(255)/255.0 alpha:1];
+
+    [button setTitle:title forState:UIControlStateNormal];
+    [self.view addSubview:button];
+
+    [button addTarget:self action:action forControlEvents:UIControlEventTouchUpInside];
+
+    return button;
+}
 
 @end
--- QLSNavTab/Classes/Three/Controller/ThreeController.m
+++ QLSNavTab/Classes/Three/Controller/ThreeController.m
@@ -26,5 +26,4 @@
     [self.view addSubview:button];
 
 }
-
 @end
